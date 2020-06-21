0015718da76707cafac168b02a39e4e6bf17e2c6
Leonard P. Soai-Van <leo.soaivan@gmail.com>
2020-06-21 13:37:56 -0400

    Add basic authenticated app

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,56 +1,68 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+} from 'formik';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const LoginForm = styled(Form)`
   display: flex;
   flex-direction: column;
 `;
 
-const Login = () => (
-  <React.Fragment>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={async (values, { setSubmitting }) => {
-        const response = await fetch('http://localhost:3000/login', {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({user: values})
-        })
+const Login = () => {
+  const { setAuthData } = useContext(AuthContext);
 
-        let token = await response.json()
+  return (
+    <React.Fragment>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={async (values, { setSubmitting }) => {
+          const response = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ user: values }),
+          });
 
-        console.log('RESPONSE: ', token)
-        setSubmitting(false);
-      }}
-    >
-      {({ isSubmitting }) => (
-        <LoginForm>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </LoginForm>
-      )}
-    </Formik>
-  </React.Fragment>
-);
+          const token = await response.json();
+
+          setAuthData(token);
+
+          console.log('RESPONSE: ', token);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <LoginForm>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </LoginForm>
+        )}
+      </Formik>
+    </React.Fragment>
+  );
+};
 
 export default Login;
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,39 @@
+import React, { createContext, useEffect, useState } from 'react';
+import Proptypes from 'prop-types';
+
+export const AuthContext = createContext({});
+
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({ loading: true, data: null });
+
+  const setAuthData = (data) => {
+    setAuth({ data });
+  };
+
+  useEffect(() => {
+    setAuth({
+      loading: false,
+      data: JSON.parse(localStorage.getItem('authData')),
+    });
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('authData', JSON.stringify(auth.data));
+  }, [auth.data]);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuthData }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: Proptypes.node,
+};
+
+AuthProvider.defaultProps = {
+  children: null,
+};
+
+export default AuthProvider;
