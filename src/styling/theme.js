5036a4cb22ab9367b576bc1a41260738afaf5174
Leonard P. Soai-Van <leo.soaivan@gmail.com>
2020-06-29 12:39:54 -0400

    Fix linting issues

--- src/styling/theme.js
+++ src/styling/theme.js
@@ -1,9 +1,9 @@
 const theme = {
   colors: {
-    powderWhite: "#FFFDF9",
-    persianGreen: "#06B49A",
-    lightBlue: "#AFDBD2",
-    onyx: "#36313D"
+    powderWhite: '#FFFDF9',
+    persianGreen: '#06B49A',
+    lightBlue: '#AFDBD2',
+    onyx: '#36313D',
   },
 };
 
